5f0b8053a2e28090371984206132860d1d9f2729
David-LBautista <ing.davlucbau@gmail.com>
2021-08-01 10:24:48 -0500

    request giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,26 +6,32 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  constructor() { }
-
+  private apiKey:string = 'fQD3daT4y7Laeulizwp1t49OXXseGbBc';
   private _historial:string[] = [];
 
-  get historial(){
-    
-    return [...this._historial];
+  constructor(
+    private http: HttpClient
+  ) { }
 
+
+  get historial(){
+    return [...this._historial];
   }
 
+
+
   buscarGifs(query:string){
-    
+
     query = query.trim().toLowerCase();
     
     if( !this._historial.includes(query) ){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
     }
-
-    console.log(this._historial)
+    this.http.get('http://api.giphy.com/v1/gifs/search?api_key=fQD3daT4y7Laeulizwp1t49OXXseGbBc&q=dbz&limit=10')
+        .subscribe( resp => {
+          console.log(resp)
+        })
   }
 
 
